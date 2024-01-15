20c0b57c205304582dc4ef24a976e45ac81c8cd8
algomonk016 <stshandilya0820@gmail.com>
2024-01-16 00:50:26 +0530

    chore: initialize react app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test.todo('the app should render');
+test.todo('it should render the input text field');
+test.todo('it should render the names dropdown on input field active');
+test.todo('it should update dropdown options on input text change');
+test.todo('it should select the first option by presseing `tab` key');
+test.todo('it should render the selected chips');
+test.todo('it should not show selected chips in dropdown');
+test.todo('it should remove last selected chip on two consecutive `backspace` press');

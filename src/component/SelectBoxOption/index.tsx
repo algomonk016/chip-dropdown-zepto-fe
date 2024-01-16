5c6db902e65a16ed5f72b0b028bb5b9e1854fbdd
algomonk016 <stshandilya0820@gmail.com>
2024-01-16 11:24:14 +0530

    feat: highlight searched text in select box

--- src/component/SelectBoxOption/index.tsx
+++ src/component/SelectBoxOption/index.tsx
@@ -5,23 +5,36 @@ import { generateIdenticon } from '../../util';
 import './selectboxOption.css';
 
 type Props = {
-  option: DropdownOption
+  option: DropdownOption,
+  highlightText?: string;
 }
 
 const SelectBoxOption: React.FC<Props> = (props) => {
-
-  const { name, email } = props.option;
+  const { option, highlightText } = props;
+  const { name, email } = option;
   const identicon = generateIdenticon(name);
+
+  const highlightSubstring = (text: string, substring: string) => {
+    if (!substring) {
+      return text;
+    }
+
+    const regex = new RegExp(`(${substring})`, 'gi');
+    return text.replace(regex, (match) => `<span class="highlighted">${match}</span>`);
+  };
+
+  const highlightedName = highlightSubstring(name, highlightText ?? "");
+  const highlightedEmail = highlightSubstring(email, highlightText ?? "");
 
   return (
     <div className='select-box-option'>
       <img src={identicon} alt={`${name}-option`} className='select-box-option-image' />
       <span>
-        <span className='select-option-name'>{name}</span>
-        <span className='select-option-email'>{email}</span>
+        <span dangerouslySetInnerHTML={{ __html: highlightedName }} className='select-option-name'></span>
+        <span dangerouslySetInnerHTML={{ __html: highlightedEmail }} className='select-option-email'></span>
       </span>
     </div>
-  )
+  );
 }
 
-export default SelectBoxOption
+export default SelectBoxOption;
